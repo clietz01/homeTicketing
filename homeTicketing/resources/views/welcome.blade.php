bff18670de3f905aca06bd5a3c6bbe718ebb065d
IASWC <123434102+melejo01@users.noreply.github.com>
2025-09-01 17:21:34 -0500

    Created scripts file for readme

--- homeTicketing/resources/views/welcome.blade.php
+++ homeTicketing/resources/views/welcome.blade.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    @vite('resources/js/app.js')
+    @inertiaHead
 </head>
 <body>
+    @inertia
     <div id="landing-container">
-        <div id="choices">
-            <a href="{{route('show_login')}}"><button>Login</button></a>
-            <a href="{{route('show_account_creation')}}"><button>Create account</button></a>
-        </div>
+    <div id="choices">
+        <a href="{{route('show_login')}}"><button>Login</button></a>
+        <a href="{{route('show_account_creation')}}"><button>Create account</button></a>
     </div>
 </body>
 </html>
---
+++ homeTicketing/scripts.md
@@ -0,0 +1,11 @@
+Inertia Scripts: 
+    composer require inertiajs/inertia-laravel
+    php artisan inertia:middleware
+    npm install @inertiajs/react
+
+Laravel Scripts:
+
+
+React Scripts:
+
+
