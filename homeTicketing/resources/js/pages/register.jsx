89eb12d398caabeb8a1159c56ec880af8b620630
IASWC <123434102+melejo01@users.noreply.github.com>
2025-09-07 18:18:48 -0500

    register.jsx

--- homeTicketing/resources/js/pages/register.jsx
+++ homeTicketing/resources/js/pages/register.jsx
@@ -1,9 +1,52 @@
-import { Form } from '@inertiajs/react'
+import { Form } from '@inertiajs/react';
+import { Link } from '@inertiajs/react';
+import { useState } from 'react';
+import { createRoot } from 'react-dom/client';
 
-export default () => (
-    <Form action="/users" method="post">
-        <input type="text" name="name" />
-        <input type="email" name="email" />
-        <button type="submit">Create User</button>
-    </Form>
-)
+export default function Register() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleNameChange(e) {
+        setName(e.target.value);
+    }
+    function handleEmailChange(e) {
+        setEmail(e.target.value);
+    }
+    function handlePasswordChange(e) {
+        setPassword(e.target.value);
+    }
+
+
+    return (
+        <form>
+            <label>Username:
+                <input
+                    type="text"
+                    value={name}
+                    onChange={handleNameChange}
+                />
+            </label>
+            <label>Email
+                <input
+                    type="text"
+                    value={email}
+                    onChange={handleEmailChange}
+                />
+            </label>
+            <label>Password
+                <input
+                    type="text"
+                    value={password}
+                    onChange={handlePasswordChange}
+                />
+            </label>
+            <Link href="/home">
+                <button type="submit">Create Account</button>
+            </Link>
+            <p></p>
+        </form >
+    )
+}
